40b47213d0558bcbd565ee27091317d13458a827
Luke Childs <lukechilds123@gmail.com>
2017-03-02 10:56:44 +0700

    Keep xo happy

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const jsdom = require('jsdom');
 // Config object must be cloned before passing through otherwise jsdom will add
 // lots of properties to the original reference.
 module.exports = class Window {
-    constructor(jsdomConfig) {
-      return jsdom.jsdom('', Object.assign({}, jsdomConfig)).defaultView;
-    }
+	constructor(jsdomConfig) {
+		return jsdom.jsdom('', Object.assign({}, jsdomConfig)).defaultView;
+	}
 };
