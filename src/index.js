f37e4fc61d2ce9fa2e0c120417d365dee2072c3a
Luke Childs <lukechilds123@gmail.com>
2017-03-02 09:06:24 +0700

    Use Object.assign instead of external clone dependency

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const jsdom = require('jsdom');
-const clone = require('clone');
 
 // Function to return a window object.
 // Accepts a jsdom config object.
 // Config object must be cloned before passing through otherwise jsdom will add
 // lots of properties to the original reference.
-module.exports = window = (jsdomConfig = {}) => jsdom.jsdom('', clone(jsdomConfig)).defaultView;
+module.exports = window = (jsdomConfig = {}) => jsdom.jsdom('', Object.assign({}, jsdomConfig)).defaultView;
